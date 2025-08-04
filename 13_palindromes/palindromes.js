3ad5c419a6df39d588d24e7e8181c7f43ecf65da
KuYunJhe <137469046+KuYunJhe@users.noreply.github.com>
2025-08-04 15:47:35 +0800

    Finish 12 & 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (s) {
 
+    s = s.replace(/[^a-zA-Z\d]/g, '').toLowerCase()
+    let arr = s.split("")
+    let arr_r = arr.slice().reverse()
+
+    console.log(arr)
+    console.log(arr_r)
+
+    return arr.every((value, index) => value === arr_r[index]);
+
+    // let arr = s.split(" ").join().split(",").join()
+    // console.log(arr)
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
