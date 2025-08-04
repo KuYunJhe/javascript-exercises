92c152365e4232f1d76be03bb62da3b9085c2359
KuYunJhe <137469046+KuYunJhe@users.noreply.github.com>
2025-08-04 16:19:47 +0800

    Finish 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+
+    let pre = 1;
+    let cur = 1;
+    let sum = 0;
+    if (num == 1) {
+        return pre;
+    }
+    else if (num == 2) {
+        return cur;
+    }
+    else if (num == 0) {
+        return 0;
+    }
+    else if (num < 0) {
+        return "OOPS"
+    }
+    else {
+        for (let i = 3; i <= num; i++) {
+            cur = pre + cur;
+            pre = cur - pre;
+        }
+        return cur;
+    }
 
 };
 
